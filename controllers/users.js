4431b81ecbcd946ff82f7931099a5101d1986a36
n00r <noor.alawadhi999@gmail.com>
2023-10-04 15:35:50 +0300

    merge sayed work

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 const User = require("../models/User")
 const Book = require("../models/Book")
 const bcrypt = require("bcryptjs")
-=======
-const User = require('../models/User')
-const Book = require('../models/Book')
-const bcrypt = require('bcryptjs')
->>>>>>> d4fead9770e6ba12cf8460037cae60ceb18957d3
 const salt = 10
 
 //Load Current User Data - HTTP GET
@@ -33,15 +27,10 @@ exports.user_show_get = async (req, res) => {
   // })
 
   try {
-<<<<<<< HEAD
     const books = await Book.find({ user: req.user._id })
       .populate("user")
       .populate("activity")
     res.render("user/detail", { books })
-=======
-    const books = await Book.find({ user: req.user._id }).populate('user')
-    res.render('user/detail', { books })
->>>>>>> d4fead9770e6ba12cf8460037cae60ceb18957d3
   } catch (err) {
     res.send(err.message)
   }
@@ -51,7 +40,7 @@ exports.user_show_get = async (req, res) => {
 exports.user_edit_get = (req, res) => {
   User.find({ id: User._id })
     .then((user) => {
-      res.render('user/edit', { user })
+      res.render("user/edit", { user })
     })
     .catch((err) => {
       console.log(err)
@@ -64,15 +53,15 @@ exports.user_update_post = (req, res) => {
   console.log(req.file)
 
   User.findByIdAndUpdate(req.body.id, {
-    userImage: '/uploads/' + req.file.filename,
+    userImage: "/uploads/" + req.file.filename,
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     phoneNumber: req.body.phoneNumber,
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
   })
     .then(() => {
-      res.redirect('/user/detail')
+      res.redirect("/user/detail")
     })
     .catch((err) => {
       console.log(err)
@@ -82,13 +71,12 @@ exports.user_update_post = (req, res) => {
 //Load change password form
 exports.user_changepass_get = async (req, res) => {
   const user = await User.findById(req.user)
-  res.render('user/changepassword', { user })
+  res.render("user/changepassword", { user })
 }
 
 //Apply Password Change
 exports.user_updatepassword_post = async (req, res) => {
   let hash = bcrypt.hashSync(req.body.password, salt)
-<<<<<<< HEAD
   // console.log(hash)
   // console.log("Password changed successfully, please re-login", req.user)
   const user = await User.findByIdAndUpdate(req.user, { password: hash })
@@ -99,10 +87,4 @@ exports.user_updatepassword_post = async (req, res) => {
     }
     res.redirect("/auth/login")
   })
-=======
-  console.log(hash)
-  console.log('Password changed successfully, please re-login', req.user)
-  const user = await User.findByIdAndUpdate(req.user, { password: hash })
-  res.redirect('/')
->>>>>>> d4fead9770e6ba12cf8460037cae60ceb18957d3
 }
