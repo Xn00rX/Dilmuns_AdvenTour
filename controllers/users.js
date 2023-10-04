d4fead9770e6ba12cf8460037cae60ceb18957d3
Ebrahim Ali <ebrahimsali99@gmail.com>
2023-10-04 14:53:45 +0300

    add search bar

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,6 @@
-const User = require("../models/User")
-const Book = require("../models/Book")
-const bcrypt = require("bcryptjs")
-const passport = require("../helper/ppConfig")
+const User = require('../models/User')
+const Book = require('../models/Book')
+const bcrypt = require('bcryptjs')
 const salt = 10
 
 //Load Current User Data - HTTP GET
@@ -28,8 +27,8 @@ exports.user_show_get = async (req, res) => {
   // })
 
   try {
-    const books = await Book.find({ user: req.user._id }).populate("user")
-    res.render("user/detail", { books })
+    const books = await Book.find({ user: req.user._id }).populate('user')
+    res.render('user/detail', { books })
   } catch (err) {
     res.send(err.message)
   }
@@ -39,7 +38,7 @@ exports.user_show_get = async (req, res) => {
 exports.user_edit_get = (req, res) => {
   User.find({ id: User._id })
     .then((user) => {
-      res.render("user/edit", { user })
+      res.render('user/edit', { user })
     })
     .catch((err) => {
       console.log(err)
@@ -52,15 +51,15 @@ exports.user_update_post = (req, res) => {
   console.log(req.file)
 
   User.findByIdAndUpdate(req.body.id, {
-    userImage: "/uploads/" + req.file.filename,
+    userImage: '/uploads/' + req.file.filename,
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     phoneNumber: req.body.phoneNumber,
     email: req.body.email,
-    password: req.body.password,
+    password: req.body.password
   })
     .then(() => {
-      res.redirect("/user/detail")
+      res.redirect('/user/detail')
     })
     .catch((err) => {
       console.log(err)
@@ -70,14 +69,14 @@ exports.user_update_post = (req, res) => {
 //Load change password form
 exports.user_changepass_get = async (req, res) => {
   const user = await User.findById(req.user)
-  res.render("user/changepassword", { user })
+  res.render('user/changepassword', { user })
 }
 
 //Apply Password Change
 exports.user_updatepassword_post = async (req, res) => {
   let hash = bcrypt.hashSync(req.body.password, salt)
   console.log(hash)
-  console.log("Password changed successfully, please re-login", req.user)
+  console.log('Password changed successfully, please re-login', req.user)
   const user = await User.findByIdAndUpdate(req.user, { password: hash })
-  res.redirect("/")
+  res.redirect('/')
 }
